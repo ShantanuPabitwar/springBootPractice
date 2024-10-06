183e577cfc48454bdf3a652e2565c6a49df0df71
shaan04 <shantanupabitwar@gmail.com>
2024-10-06 17:15:54 +0530

    oneToMany Relationship

---
+++ src/main/java/com/eazybytes/eazyschool/controller/ProfileController.java
@@ -0,0 +1,67 @@
+package com.eazybytes.eazyschool.controller;
+
+import com.eazybytes.eazyschool.model.Address;
+import com.eazybytes.eazyschool.model.Person;
+import com.eazybytes.eazyschool.model.Profile;
+import com.eazybytes.eazyschool.repository.PersonRepository;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+@Controller
+public class ProfileController {
+
+    @Autowired
+    PersonRepository personRepository;
+
+    @RequestMapping("/displayProfile")
+    public ModelAndView displayProfile(Model model, HttpSession session){
+        Person person = (Person) session.getAttribute("loggedInPerson");
+        Profile profile = new Profile();
+        profile.setName(person.getName());
+        profile.setEmail(person.getEmail());
+        profile.setMobileNumber(person.getMobileNumber());
+        if(person.getAddress() != null && person.getAddress().getAddressId() > 0){
+            profile.setAddress1(person.getAddress().getAddress1());
+            profile.setAddress2(person.getAddress().getAddress2());
+            profile.setCity(person.getAddress().getCity());
+            profile.setState(person.getAddress().getState());
+            profile.setZipCode(person.getAddress().getZipCode());
+        }
+         ModelAndView modelAndView = new ModelAndView("profile.html");
+        modelAndView.addObject("profile", profile);
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/updateProfile", method = RequestMethod.POST)
+    public String updateProfile(@Valid @ModelAttribute("profile") Profile profile, Errors errors, HttpSession session){
+        if (errors.hasErrors()){
+            return  "profile.html";
+        }
+        Person person = (Person) session.getAttribute("loggedInPerson");
+        person.setName(profile.getName());
+        person.setMobileNumber(profile.getMobileNumber());
+        person.setEmail(profile.getEmail());
+        if(person.getAddress() == null || !(person.getAddress().getAddressId() > 0)){
+            person.setAddress(new Address());
+        }
+        assert person.getAddress() != null;
+        person.getAddress().setAddress1(profile.getAddress1());
+        person.getAddress().setAddress2(profile.getAddress2());
+        person.getAddress().setCity(profile.getCity());
+        person.getAddress().setZipCode(profile.getZipCode());
+        person.getAddress().setState(profile.getState());
+        personRepository.save(person);
+        session.setAttribute("loggedInPerson", person);
+        return "redirect:/displayProfile";
+    }
+}
---
+++ src/main/java/com/eazybytes/eazyschool/model/Profile.java
@@ -0,0 +1,40 @@
+package com.eazybytes.eazyschool.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class Profile {
+    @NotBlank(message = "Name must not be blank")
+    @Size(min = 3, message = "Name muse be at lease 3 characters long")
+    private  String name;
+
+    @NotBlank(message = "Mobile number must not be blank")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+    private String mobileNumber;
+
+    @NotBlank(message = "Email muse not be blank")
+    @Email(message = "Please provide valid email address")
+    private  String email;
+
+    @NotBlank(message = "Address1 must not be blank")
+    @Size(min = 5, message = "Address1 must be at least 5 character long")
+    private String address1;
+
+    private String address2;
+
+    @NotBlank(message = "city must not be blank")
+    @Size(min = 5, message = "city must be at least 5 character long")
+    private String city;
+
+    @NotBlank(message = "state must not be blank")
+    @Size(min = 5, message = "State must be at least 5 character long")
+    private String state;
+
+    @NotBlank(message = "Zip code must not be blank")
+    @Pattern(regexp = "(^$|[0-9]{5})", message = "Zip code must be 5 digits")
+    private String zipCode;
+}
