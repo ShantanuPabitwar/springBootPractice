51ba323507a94423517d58a7134ac9801306b98b
shaan04 <shantanupabitwar@gmail.com>
2024-10-08 00:45:19 +0530

    @Query @NamedQuery and @NamedNativeQuery

--- src/main/java/com/eazybytes/eazyschool/repository/ContactRepository.java
+++ src/main/java/com/eazybytes/eazyschool/repository/ContactRepository.java
@@ -1,7 +1,14 @@
 package com.eazybytes.eazyschool.repository;
 
 import com.eazybytes.eazyschool.model.Contact;
+import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,8 +19,30 @@ type to the Spring context and indicate that given Bean is used to perform
 DB related operations and
 * */
 @Repository
-public interface ContactRepository extends CrudRepository<Contact, Integer> {
+public interface ContactRepository extends JpaRepository<Contact, Integer> {
 
     List<Contact> findByStatus(String status);
 
+//    @Query("SELECT c FROM Contact c WHERE c.status = :status")
+    @Query(value = "SELECT * FROM contact_msg c WHERE c.status = :status", nativeQuery = true)
+    Page<Contact> findByStatus(String status, Pageable pageable);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Contact c SET c.status = ?1 WHERE c.contactId = ?2")
+    int updateContactById(String status, int Id);
+
+    Page<Contact> findOpenStatus(@Param("status") String status, Pageable pageable);
+
+    @Modifying
+    @Transactional
+    int closeStatus(int id);
+
+    @Query(nativeQuery = true)
+    Page<Contact> findOpenMsgsNative(@Param("status") String status, Pageable pageable);
+
+    @org.springframework.transaction.annotation.Transactional
+    @Modifying
+    @Query(nativeQuery = true)
+    int updateMsgStatusNative(String status, int id);
 }
